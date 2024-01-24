9de2fb6025badccd440c20b7d9281d6e48de1081
Volodymyr Starodubov <volodymyr.starodubov@alpacked.io>
2024-01-24 17:42:29 +0200

    Fixes to vault init script

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -4,50 +4,54 @@ API_SERVER="https://kubernetes.default.svc"
 SA_TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
 SA_NAMESPACE="$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
 LEADER_VAULT_ADDRESS="http://vault-0.vault-internal.${SA_NAMESPACE}:8200"
+LEADER_SECRET_THRESHOLD=3
+LEADER_SECRET_SHARES=5
 
 get_vault_pod_list() {
   curl -k -X GET \
-      -H "Authorization: Bearer $SA_TOKEN" \
+      -H "Authorization: Bearer ${SA_TOKEN}" \
       -H "Accept: application/json" \
-      "$APISERVER/api/v1/namespaces/$SA_NAMESPACE/pods" | jq -r '.items[].metadata.name | select(test("vault-agent-injector") == false)'
+      "${API_SERVER}/api/v1/namespaces/${SA_NAMESPACE}/pods?labelSelector=component%3Dserver" | jq -r '.items[].metadata.name'
 }
 
 save_secrets() {
-  local unseal_tokens=""
+  local count=0
+  local unseal_tokens="{}"
 
-  for i in $(seq 0 $(($(echo "$LEADER_RESPONSE" | jq -r '.keys_base64 | length') - 1))); do
-    count=$((i+1))
-    unseal_tokens+="\"unseal_token_$count\": \"$(echo "$LEADER_RESPONSE" | jq -r ".keys_base64[$i]")\""
-    if [ $i -lt $(($(echo "$LEADER_RESPONSE" | jq -r '.keys_base64 | length') - 1)) ]; then
-      unseal_tokens+=", "
-    fi
-  done
+  while read -r token; do
+    count=$((count+1))
+    token=$(echo -n "$token" | base64 -w 0)
+    unseal_tokens=$(echo "$unseal_tokens" | jq --arg count "$count" --arg token "$token" '. + {("unseal_token_" + $count): $token}')
+  done < <(echo "${LEADER_RESPONSE}" | jq -r '.keys[]')
 
-  curl -k -X POST \
-      -H "Authorization: Bearer $SA_TOKEN" \
-      -H "Content-Type: application/json" \
-      --data '{
+  local json_payload=$(jq -n \
+    --argjson unsealTokens "${unseal_tokens}" \
+    --arg rootToken "$(echo -n "${LEADER_RESPONSE}" | jq -rj ".root_token" | base64 -w 0)" \
+    --arg namespace "${SA_NAMESPACE}" \
+    '{
         "apiVersion": "v1",
         "kind": "Secret",
         "metadata": {
           "name": "vault-init-secrets",
-          "namespace": "'"$SA_NAMESPACE"'"
+          "namespace": $namespace
         },
         "type": "Opaque",
-        "data": {
-          "root_token": "'"$(echo "$LEADER_RESPONSE" | jq -r ".root_token" | base64 -w 0)"'",
-          '"$unseal_tokens"'
-        }
-      }' \
-      "$API_SERVER/api/v1/namespaces/$SA_NAMESPACE/secrets"
+        "data": ({"root_token": $rootToken} + $unsealTokens)
+      }'
+  )
+
+  curl -s -k -X POST \
+      -H "Authorization: Bearer ${SA_TOKEN}" \
+      -H "Content-Type: application/json" \
+      --data "${json_payload}" \
+      "${API_SERVER}/api/v1/namespaces/${SA_NAMESPACE}/secrets"
+
+  echo "Saving tokens to vault-init-secrets..."
 }
 
 initialize_vault() {
-  local vault_secret_threshold=3
-  local vault_secret_shares=5
-
   LEADER_RESPONSE=$(curl -s -X POST \
-                        --data "{\"secret_shares\": ${vault_secret_shares}, \"secret_threshold\": ${vault_secret_threshold}}" \
+                        --data "{\"secret_shares\": ${LEADER_SECRET_SHARES}, \"secret_threshold\": ${LEADER_SECRET_THRESHOLD}}" \
                         "${LEADER_VAULT_ADDRESS}/v1/sys/init")
 
   if [ "$(curl -s "${LEADER_VAULT_ADDRESS}/v1/sys/health" | jq -r '.initialized')" = "true" ]; then
@@ -59,12 +63,13 @@ initialize_vault() {
   fi
 
   echo "Trying to unseal the Vault..."
-  for i in $(seq 0 $((vault_secret_threshold - 1))); do
+  for i in $(seq 0 $((LEADER_SECRET_THRESHOLD - 1))); do
       local unseal_key=$(echo "$LEADER_RESPONSE" | jq -r ".keys[$i]")
       local unseal_response=$(curl -s -X POST \
                                 --data "{\"key\": \"$unseal_key\"}" \
                                 "${LEADER_VAULT_ADDRESS}/v1/sys/unseal")
       echo "Keys applied: $((i+1))"
+      sleep 1 # Waiting to prevent spam
   done
 
   if [ "$(curl -s "${LEADER_VAULT_ADDRESS}/v1/sys/health" | jq -r '.sealed')" = "false" ]; then
@@ -80,39 +85,41 @@ initialize_raft_vault() {
   local raft_vault_address="http://${pod_name}.vault-internal.${SA_NAMESPACE}:8200"
 
   local raft_response=$(curl -s -X POST \
-                        --header "X-Vault-Token: $(echo "$LEADER_RESPONSE" | jq -r ".root_token")" \
-                        --data "{\"leader_api_addr\": ${LEADER_VAULT_ADDRESS}}" \
+                        --header "X-Vault-Token: $(echo "${LEADER_RESPONSE}" | jq -rj ".root_token")" \
+                        --data "{\"leader_api_addr\": \"${LEADER_VAULT_ADDRESS}\"}" \
                         "${raft_vault_address}/v1/sys/storage/raft/join")
 
   if [ "$(curl -s "${raft_vault_address}/v1/sys/health" | jq -r '.initialized')" = "true" ]; then
-      echo "Raft Vault $pod_name is initialized."
+      echo "Raft $pod_name is initialized. Waiting 5 seconds to estabilish connection with master node..."
+      sleep 5
   else
-      echo "Raft Vault $pod_name is not initialized."
+      echo "Raft $pod_name is not initialized."
       exit 1
   fi
 
-  echo "Trying to unseal the $pod_name raft Vault..."
-  for i in $(seq 0 $((vault_secret_threshold - 1))); do
+  echo "Trying to unseal the raft $pod_name..."
+  for i in $(seq 0 $((LEADER_SECRET_THRESHOLD - 1))); do
       local unseal_key=$(echo "$LEADER_RESPONSE" | jq -r ".keys[$i]")
       local unseal_response=$(curl -s -X POST \
                                 --data "{\"key\": \"$unseal_key\"}" \
                                 "${raft_vault_address}/v1/sys/unseal")
       echo "Keys applied: $((i+1))"
+      sleep 1 # Waiting to prevent spam
   done
 
   if [ "$(curl -s "${raft_vault_address}/v1/sys/health" | jq -r '.sealed')" = "false" ]; then
-    echo "Raft Vault $pod_name is unsealed."
+    echo "Raft $pod_name is unsealed."
   else
-      echo "Raft Vault $pod_name is still sealed."
+      echo "Raft $pod_name is still sealed."
       exit 1
   fi
 }
 
 vault_pod_list=$(get_vault_pod_list)
-for pod in $pod_list; do
+for pod in $vault_pod_list; do
   if [ "$pod" == "vault-0" ]; then
       initialize_vault
-  elif
+  else
       initialize_raft_vault $pod
   fi
 done
